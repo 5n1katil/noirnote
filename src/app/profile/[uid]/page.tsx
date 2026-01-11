0fec7a5d540fc23eb1c44c96ba074c6a0fa2b82d
5N1Dedektif <5n1katildedektiflikoyunu@gmail.com>
2026-01-11 21:14:37 +0300

    fix: Remove Firebase client SDK usage from server component in profile/[uid]/page

--- src/app/profile/[uid]/page.tsx
+++ src/app/profile/[uid]/page.tsx
@@ -2,9 +2,6 @@ import AuthGate from "@/components/AuthGate";
 import { AuthedShell } from "@/components/AuthedShell";
 import { textsTR } from "@/lib/texts.tr";
 import PublicProfileClient from "./PublicProfileClient";
-import { notFound } from "next/navigation";
-import { getUserDoc } from "@/lib/userDoc.client";
-import { getFirebaseClient } from "@/lib/firebase.client";
 
 type PublicProfilePageProps = {
   params: Promise<{ uid: string }>;
@@ -12,16 +9,10 @@ type PublicProfilePageProps = {
 
 export default async function PublicProfilePage({ params }: PublicProfilePageProps) {
   const { uid } = await params;
-  
-  // Verify user exists
-  const userDoc = await getUserDoc(uid);
-  if (!userDoc) {
-    notFound();
-  }
 
   return (
     <AuthGate>
-      <AuthedShell title={userDoc.detectiveUsername || textsTR.profile.title}>
+      <AuthedShell title={textsTR.profile.title}>
         <PublicProfileClient targetUid={uid} />
       </AuthedShell>
     </AuthGate>
